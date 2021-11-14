3c2e7229584ba2c784f800469b3bd178050fe25c
Resul <68106627+SamedovResul@users.noreply.github.com>
2021-11-14 16:55:40 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const imran = document.querySelector('.imran-money');
+const kabira = document.querySelector('.Kabira-money');
+const Farahnaz = document.querySelector('.Farahnaz-money');
+const Raul = document.querySelector('.Raul-money');
+const Davud = document.querySelector('.Davud-money');
+const Oqtay = document.querySelector('.Oqtay-money');
+const Reyhana = document.querySelector('.Reyhana-money');
+
+
+imran.innerHTML = 0
+kabira.innerHTML = 0
+Farahnaz.innerHTML = 0
+Raul.innerHTML = 0
+Davud.innerHTML = 0
+Oqtay.innerHTML = 0
+Reyhana.innerHTML = 0
